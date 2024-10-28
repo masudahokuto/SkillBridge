1345b5cae63bd34c27ac737f70a52826156df2ee
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-10-28 07:03:22 +0000

    user[add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :companies
-  devise_for :admins
-  devise_for :users
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :companies,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about', to: 'homes#about'
+    resources :users, except: %i[new create] do
+      collection do
+        get 'mypage', to: 'users#mypage'
+        patch 'withdraw'  # 退会処理
+      end      
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
