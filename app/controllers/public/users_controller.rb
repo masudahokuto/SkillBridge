4e37533842583c9608c904577e5228c1bc906c22
EC2 Default User <mendokuse_365@i.softbank.jp>
2024-10-31 13:13:22 +0000

    user-edit[add]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,8 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :redirect_if_not_current_user, only: [:edit, :update]
+
   def index
   end
 
@@ -6,7 +10,14 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to mypage_users_path, notice: 'プロフィールが更新されました'
+    else
+      render :edit
+    end
   end
 
   def mypage
@@ -14,6 +25,14 @@ class Public::UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def redirect_if_not_current_user
+    redirect_to posts_path unless current_user == @user
+  end
 
   def user_params
     params.require(:user).permit(:nick_name, :profile, :gender, :location, :birthday, :email, :password,  :profile_image)
