027b0595315873b1f219890c6e685b9da3cc071a
EC2 Default User <mendokuse_365@i.softbank.jp>
2024-11-04 09:44:21 +0000

    tag-new[add]

--- app/controllers/public/skill_tags_controller.rb
+++ app/controllers/public/skill_tags_controller.rb
@@ -2,4 +2,19 @@ class Public::SkillTagsController < ApplicationController
   def index
     @skill_tags = SkillTag.all
   end
+
+  def create
+    @skill_tag = SkillTag.new(skill_tag_params)
+    if @skill_tag.save
+      redirect_to new_portfolio_path, notice: 'スキルタグが作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def skill_tag_params
+    params.require(:skill_tag).permit(:name, :skill_tag_image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
     resources :portfolios
 
-    resources :skill_tags, only: %i[index]
+    resources :skill_tags, only: %i[index create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
