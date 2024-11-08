b98eb0d02ce1f3e4981fd94b7362979d68da4857
EC2 Default User <mendokuse_365@i.softbank.jp>
2024-11-08 13:13:05 +0000

    script.js[add]

---
+++ app/javascript/script.js
@@ -0,0 +1,20 @@
+// 3秒後にフラッシュメッセージをフェードアウト
+setTimeout(function() {
+  var flashMessages = document.getElementById('flash-messages');
+  if (flashMessages) {
+    flashMessages.style.transition = 'opacity 0.5s ease';
+    flashMessages.style.opacity = '0';
+    setTimeout(function() {
+      flashMessages.remove();
+    }, 500);
+  }
+}, 3000);
+
+// $(document).on('turbolinks:load', function() {
+//   $('#back').on('click', function(event) {
+//     $('body, html').animate({
+//       scrollTop: 0
+//     }, 500);
+//     event.preventDefault();
+//   });
+// });
